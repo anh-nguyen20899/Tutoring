2c778332f327259f725714736050d3d1509b45af
anhng <anananh1999@gmail.com>
2024-04-11 20:44:28 +1000

    create Today plan

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -14,20 +14,60 @@ const DashboardScreen = ({navigation}) =>
             },
             ]}>
             <View style={{flex: 1, backgroundColor: 'white'}} >
-                <Text> Today </Text>
+                <Text style={styles.baseText}> Today </Text>
             </View>
-            <View style={{flex: 2, backgroundColor: 'red'}}> 
-                <Text> 7 Math
-                8:20 Learning Algebra
-                </Text>
+            <View style={{flex: 2, backgroundColor: 'red', flexDirection: 'column'}}> 
+                <View>
+                    <Text style={styles.innerText}> 07:00 Math </Text>
+                </View>
+                <View>
+                    <Text style={styles.innerText}> 08:20 Learning Algebra </Text>
+                </View>
+                
             </View>
             
-            <View style={{flex: 3, backgroundColor: 'darkorange'}}>
-            <Text> 7 English
-                8:20 Expanding Vocabulary
-                </Text>
+            <View style={{flex: 2, backgroundColor: 'darkorange'}}>
+                <View>
+                    <Text style={styles.innerText}> 07:00 Math </Text>
+                </View>
+                <View>
+                    <Text style={styles.innerText}> 08:20 Learning Algebra </Text>
+                </View>
              </View>
-            
+
+             <View
+                style={[
+                styles.container,
+                {
+                    // Try setting `flexDirection` to `"row"`.
+                    flexDirection: 'row',
+                },
+                ]}>
+                    <View style={{flex: 1, backgroundColor: 'white'}} >
+                        <Text> This week plan </Text>
+                    </View>
+                    <View style={{flex: 1, backgroundColor: 'white'}} >
+                        <Text> Last week </Text>
+                    </View>
+            </View>
+             
+                {/* <View style={{flex: 1, backgroundColor: 'white'}} >
+                    <Text> Today </Text>
+                </View>
+                <View style={{flex: 2, backgroundColor: 'red'}}> 
+                    <Text> 7 Math
+                    8:20 Learning Algebra
+                    </Text>
+                </View>
+                
+                <View style={{flex: 3, backgroundColor: 'darkorange'}}>
+                <Text> 7 English
+                    8:20 Expanding Vocabulary
+                    </Text>
+                </View>
+                <View style={{ backgroundColor: 'white'}} >
+                    <Text> Today </Text>
+                </View> */}
         </View>
         
     )
@@ -37,5 +77,13 @@ const styles = StyleSheet.create({
       flex: 1,
       padding: 20,
     },
+    baseText: {
+        fontWeight: 'bold',
+      },
+      innerText: {
+        color: 'white',
+        fontWeight: "bold",
+        fontSize: 20
+      },
   });
 export default DashboardScreen;
